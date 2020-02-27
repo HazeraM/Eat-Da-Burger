671a905ff8c02b2f9f67d24c31dbba8e489b8562
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-02-26 20:49:10 -0500

    created mysql connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,13 @@ var mysql = require("mysql");
 // to protect the key from github
 require("dotenv").config();
 
-
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
 
 // Export connection for our ORM to use.
 module.exports = connection; 
---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Imposible Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple-Stacked Avocado", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Doubel Whopper", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("South of the Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Wild Buffalo Chicken Burger", FALSE);
