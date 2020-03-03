1dff45fcface453c3f9d3dae62dac254b4ff5559
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-03-03 18:39:07 -0500

    added heroku link

--- config/connection.js
+++ config/connection.js
@@ -3,24 +3,29 @@ var mysql = require("mysql");
 // to protect the key from github
 require("dotenv").config();
 
-var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.KEY,
+    database: "burgers_db"
+  });
+};
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.KEY,
-  database: "burgers_db"
-});
+  
 
 // Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
-module.exports = connection; 
+module.exports = connection;
